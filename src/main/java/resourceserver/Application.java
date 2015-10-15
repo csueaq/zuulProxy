1291f5c57ede9d22e862c2ad1b625aeb43adc6c7
Xin Jin <xin.jin@net-a-porter.com>
2015-10-15 12:28:41 +0100

    initial commit

---
+++ src/main/java/resourceserver/Application.java
@@ -0,0 +1,20 @@
+package resourceserver;
+
+/**
+ * Created by Izzy on 17/08/15.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableZuulProxy
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
